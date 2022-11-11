d5b5d6edbbc1b0b6899ee54a86f16e33a4c38e1c
JFEstevezC <jose2224646@correo.uis.edu.co>
2022-11-11 18:08:52 -0500

    se agrego el codigo con su interfaz

--- Numero_4_digitos_positivo.py
+++ Numero_4_digitos_positivo.py
@@ -44,55 +44,60 @@ x = StringVar()
 # frame entrada datos
 # -------------------
 frame_entrada = Frame(ventana_principal)
-frame_entrada.config(bg="green2", width=480, height=240)
+frame_entrada.config(bg="green", width=480, height=240)
 frame_entrada.place(x=10, y=10)
+
+logo = PhotoImage(file="img/Logo.png")
+lb_logo = Label(frame_entrada, image=logo)
+lb_logo.place(x=10, y=120)
+
 
 # etiqueta para  el título de la app
 titulo = Label(frame_entrada, text= "¿Es positivo de 4 digitos?")
-titulo.config(bg="green2", fg="blue", font=("Arial",16))
-titulo.place(x=120,y=5)
+titulo.config(bg="green", fg="black", font=("Arial",25))
+titulo.place(x=50,y=5)
 
 # Caja de entrada de texto
-tx = Label(frame_entrada, text="Escriba el número para el cuál desea saber si es un número\n de 4 digitos positivo o no ")
-tx.config(bg="green", fg="blue", font=("Times New Roman",12))
-tx.place(x=20, y=50, width=500, height=70)
+tx = Label(frame_entrada, text="Digite el número para el cuál desea saber si\n es un número de 4 digitos positivo o no ")
+tx.config(bg="green", fg="black", font=("Times New Roman",16))
+tx.place(x=30, y=50, width=420, height=70)
 
 # Entrada
 entry_x = Entry(frame_entrada, textvariable=x)
-entry_x.config(bg="White", fg="black", font=("Times New Roman",16))
+entry_x.config(bg="White", fg="black", font=("Times New Roman",20))
 entry_x.focus_set()
-entry_x.place(x=190, y=160, width=100, height=30)
+entry_x.place(x=200, y=160, width=100, height=30)
 
 # frame operaciones
 
 frame_operaciones = Frame(ventana_principal)
-frame_operaciones.config(bg="blue", width=480, height=120)
-frame_operaciones.place(x=10, y=250)
+frame_operaciones.config(bg="green", width=480, height=120)
+frame_operaciones.place(x=10, y=260)
 
 # Botón Sumar
 bt_comprobar = Button(frame_operaciones, text="Comprobar", command=comprobar)
 bt_comprobar.config(bg="white",  fg="black", font=("Times New Roman",16))
-bt_comprobar.place(x=45, y=45, width=100, height=50)
+bt_comprobar.place(x=45, y=35, width=100, height=50)
 
 # Botón Borrar
 bt_borrar = Button(frame_operaciones, text="Borrar", command=borrar)
 bt_borrar.config(bg="white", fg="black", font=("Times New Roman",16))
-bt_borrar.place(x=190, y=45, width=100, height=50)
+bt_borrar.place(x=190, y=35, width=100, height=50)
 
 # Botón Salir
 bt_salir = Button(frame_operaciones, text="Salir", command=salir)
 bt_salir.config(bg="white", fg="black", font=("Times New Roman",16))
-bt_salir.place(x=335, y=45, width=100, height=50)
+bt_salir.place(x=335, y=35, width=100, height=50)
 
 # frame resultados
 frame_resultados = Frame(ventana_principal)
-frame_resultados.config(bg="red", width=480, height=120)
+frame_resultados.config(bg="black", width=480, height=120)
 frame_resultados.place(x=10, y=370)
 
 # Caja de salida de texto
 t_resultados = Text(frame_resultados)
-t_resultados.config(bg="lime green", fg="white", font=("Times New Roman",16))
-t_resultados.place(x=10, y=10, width=460, height=80)
+t_resultados.config(bg="green", fg="white", font=("Times New Roman",20))
+t_resultados.place(x=0, y=10, width=480, height=100)
 
 # Se ejecuta el método mainloop() de la clase Tk a través de la instancia ventana principal. Este método despliega una ventana simple en la pantalla y queda a la espera de lo que el usuario haga (click en un botón, escribir, etc) Cada acción del usuario se conoce como un evento. El método mainloop() es un bucle infinito.
 ventana_principal.mainloop()
